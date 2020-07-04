bc07c38c2d02d1903eb744df8275a12f72b8648c
扶 <1972257838@qq.com>
2020-07-04 11:55:34 +0800

    git练习

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("sssssssssswwwwwwwwwwwww");
+        System.out.println("1111111111111111");
     }
 }
